f370d5dc186a36088d401712fa719cf17bb8722f
dev-leuzaoo <ofurtivo2019@gmail.com>
2025-05-13 15:07:52 -0300

    Updated InputField layout style

--- frontend/src/components/ui/InputField.tsx
+++ frontend/src/components/ui/InputField.tsx
@@ -9,11 +9,11 @@ type Props = {
 function InputField({ label, placeholder, type }: Props) {
   return (
     <div className="flex flex-col w-full min-w-xs">
-      <label className="text-lg font-light">{label}</label>
+      <label className="font-light">{label}</label>
       <input
         type={type}
         placeholder={placeholder}
-        className="bg-light w-full py-2 text-lg text-dark pl-2 rounded-md outline-none placeholder:text-base"
+        className="bg-light w-full py-1 text-lg text-dark pl-2 rounded-md outline-none placeholder:text-base"
       />
     </div>
   );
