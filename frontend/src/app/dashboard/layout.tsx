30a37a92a93d61ea3ae898db540ae7a5ffe30e76
leuzaoo <leuzaoocontato@gmail.com>
2025-05-23 12:56:19 -0300

    Created layout and dashboard page

---
+++ frontend/src/app/dashboard/layout.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import Sidebar from "../../components/ui/Sidebar";
+
+export default function Layout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <div className="flex h-screen w-full">
+      <div>
+        <Sidebar />
+      </div>
+      <main className="flex-1 overflow-y-auto p-4">{children}</main>
+    </div>
+  );
+}
