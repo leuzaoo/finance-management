e14e474afb23fdc92972f1354ad694dcffbd44cd
dev-leuzaoo <154552904+dev-leuzaoo@users.noreply.github.com>
2025-05-27 07:00:42 -0300

    Fixed validate function adding `:void`
    Co-authored-by: Leonardo Costa <94720093+leuzaoo@users.noreply.github.com>

--- backend/src/routes/bank.route.ts
+++ backend/src/routes/bank.route.ts
@@ -10,7 +10,7 @@ import {
 } from "../controllers/bank.controller";
 
 const router = Router();
-const validate = (req: Request, res: Response, next: NextFunction) => {
+const validate = (req: Request, res: Response, next: NextFunction): void => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     res.status(400).json({ errors: errors.array() });
