b31b67b4d4d47d68388e448603fff19ad3714f7c
dev-leuzaoo <154552904+dev-leuzaoo@users.noreply.github.com>
2025-05-28 14:04:03 -0300

    fix: update layout link button from homepage
    Co-authored-by: Leonardo Costa <94720093+leuzaoo@users.noreply.github.com>

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <>
       <div className="flex min-h-screen flex-col items-center justify-center">
         <h1 className="text-3xl font-bold">Bem-vindo!</h1>
-        <Link href="/login" className="bg-blue-400 p-3">
+        <Link href="/login" className="bg-blue-600 px-6 py-2">
           Ir para login
         </Link>
       </div>
