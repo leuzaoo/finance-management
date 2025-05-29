17186a6a2eef087812e906e3870e3ea151de5f46
dev-leuzaoo <ofurtivo2019@gmail.com>
2025-05-29 07:55:26 -0300

    feat: created Transaction model schema

---
+++ backend/src/models/Transaction.ts
@@ -0,0 +1,32 @@
+import { model, Schema, type Types } from "mongoose";
+
+export type TransactionType = "expense" | "income" | "transfer";
+
+export interface ITransaction extends Document {
+  bank: Types.ObjectId;
+  type: TransactionType;
+  amount: number;
+  description?: string;
+  category: string;
+  date: Date;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const TransactionSchema = new Schema<ITransaction>(
+  {
+    bank: { type: Schema.Types.ObjectId, ref: "Bank", required: true },
+    type: {
+      type: String,
+      required: true,
+      enum: ["expense", "income", "transfer"],
+    },
+    amount: { type: Number, required: true },
+    description: { type: String },
+    category: { type: String, required: true },
+    date: { type: Date, required: true, default: () => new Date() },
+  },
+  { timestamps: true }
+);
+
+export default model<ITransaction>("Transaction", TransactionSchema);
