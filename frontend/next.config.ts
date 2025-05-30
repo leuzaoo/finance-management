709bfc1d6f8b04a12ed5774a7adb4300044dbc79
dev-leuzaoo <ofurtivo2019@gmail.com>
2025-05-30 09:48:07 -0300

    feat: added source rule to next.config to get infos  from backend in dev environment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -12,6 +12,10 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
+        source: "/api/v1/:path*",
+        destination: "http://localhost:5000/api/v1/:path*",
+      },
+      {
         source: "/api/:path*",
         destination: "https://finance-management-ncdf.onrender.com/api/:path*",
       },
