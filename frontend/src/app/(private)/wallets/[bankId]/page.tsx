d5d50f883e24c820066b4673efbff4438e1fac8b
dev-leuzaoo <ofurtivo2019@gmail.com>
2025-05-30 10:48:09 -0300

    fix: type of property "params" are incompatible

--- frontend/src/app/(private)/wallets/[bankId]/page.tsx
+++ frontend/src/app/(private)/wallets/[bankId]/page.tsx
@@ -1,14 +1,14 @@
+import * as React from "react";
 import { redirect } from "next/navigation";
 
 import WalletDetailsClient from "./WalletsDetailsClient";
 
-interface PageProps {
-  params: { bankId: string };
-}
-
-export default async function WalletDetailsPage({ params }: PageProps) {
-  // mesmo que tenha um aviso no 'await' abaixo, de acordo com a documentacao do next, precisa usar await
-  const { bankId } = await params;
+export default function WalletDetailsPage({
+  params,
+}: {
+  params: Promise<{ bankId: string }>;
+}) {
+  const { bankId } = React.use(params);
 
   if (!bankId) return redirect("/dashboard");
   return <WalletDetailsClient bankId={bankId} />;
