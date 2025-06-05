91b857d30f6ef6b10a3d0cc0fa817c572891e32e
dev-leuzaoo <ofurtivo2019@gmail.com>
2025-06-05 12:28:56 -0300

    feat: created RegisterAndLoginBg componente to render image com auth pages

---
+++ frontend/src/components/ui/RegisterAndLoginBg.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import React from "react";
+
+const RegisterAndLoginBg = () => {
+  return (
+    <Image
+      src="https://images.unsplash.com/photo-1579532537598-459ecdaf39cc?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      alt="Background image"
+      height={1920}
+      width={1080}
+    />
+  );
+};
+
+export default RegisterAndLoginBg;
