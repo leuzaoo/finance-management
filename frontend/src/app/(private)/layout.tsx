a6ee6ee61023458ce889ac546d81dbe79ab9d470
dev-leuzaoo <ofurtivo2019@gmail.com>
2025-06-12 10:24:40 -0300

    feat: removed useless lines from dashboard layout file

--- frontend/src/app/(private)/layout.tsx
+++ frontend/src/app/(private)/layout.tsx
@@ -7,9 +7,7 @@ export default function Layout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <div className="flex h-screen w-full">
-      <div>
-        <Sidebar />
-      </div>
+      <Sidebar />
       <main className="bg-light/10 flex-1 overflow-y-auto p-4">{children}</main>
     </div>
   );
