0f640d9a486980829eeef7880f14b7e05f3d4191
leuzaoo <leuzaoocontato@gmail.com>
2025-06-24 07:06:22 -0300

    feat: add Reminder model with schema and interface

---
+++ backend/src/models/Reminder.ts
@@ -0,0 +1,24 @@
+import mongoose, { Document, model, Schema } from "mongoose";
+
+export interface IReminder extends Document {
+  user: mongoose.Types.ObjectId;
+  title: string;
+  description: string;
+  date: Date;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const ReminderSchema: Schema = new Schema(
+  {
+    user: { type: mongoose.Types.ObjectId, ref: "User", required: true },
+    title: { type: String, required: true, trim: true },
+    description: { type: String, required: true },
+    date: { type: Date, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default model<IReminder>("Reminder", ReminderSchema);
