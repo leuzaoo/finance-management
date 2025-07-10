4c21ada6be20baab91cfe3acf61901fa4fef9977
dev-leuzaoo <ofurtivo2019@gmail.com>
2025-07-10 10:06:44 -0300

    feat: enhance Home component with dashboard images for better user experience

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,38 @@
+import Image from "next/image";
+
 import HeroSection from "../components/ui/Homepage/HeroSection";
 import Navbar from "../components/ui/Homepage/Navbar";
 
 export default function Home() {
   return (
     <>
-      <main className="h-screen w-full bg-gray-200">
+      <main className="h-full w-full bg-gray-200">
         <Navbar />
         <HeroSection />
+        <section>
+          <h1 className="sr-only">Dashboard Image</h1>
+          <div className="container mx-auto mt-10 px-4 pb-5 md:px-0">
+            <Image
+              src="/dashboard-mobile-print.png"
+              alt="Dashboard Print"
+              width={1080}
+              height={1920}
+              className="mx-auto rounded-md sm:hidden"
+            />
+
+            <Image
+              src="/dashboard-print.png"
+              alt="Dashboard Print"
+              width={1920}
+              height={1080}
+              className="mx-auto hidden rounded-lg sm:block"
+            />
+            <p className="text-dark mt-2 px-4 text-center text-sm opacity-50">
+              Imagens reais do dashboard. Design e códigos feito apenas por uma
+              pessoa.
+            </p>
+          </div>
+        </section>
       </main>
     </>
   );
