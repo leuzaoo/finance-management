9816b064b53d4cd3318293f1ac5040f6558bfc8e
leuzaoo <leuzaoocontato@gmail.com>
2025-07-17 18:20:15 -0300

    fix: removed useless lines from layout and page main app files

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,13 +1,9 @@
-"use client";
-import { useEffect } from "react";
 import { Archivo, Inter } from "next/font/google";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 
 import "react-toastify/dist/ReactToastify.css";
 import "./globals.css";
-
-import { useTheme } from "../hooks/useTheme";
 
 const zonaProBold = localFont({
   src: "../fonts/ZonaPro-Bold.otf",
@@ -31,39 +27,21 @@ const archivo = Archivo({
   weight: ["200", "400", "500", "600", "700", "900"],
 });
 
-// todo
-// export const metadata: Metadata = {
-//   title: "Gerenciador de Finanças",
-//   description:
-//     "Administre suas finanças de forma fácil e simples. O resto deixe conosco.",
-// };
+export const metadata: Metadata = {
+  title: "FinSafe | Gerenciador de Finanças",
+  description:
+    "Administre suas finanças de forma fácil e simples. O resto deixe conosco.",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const { theme } = useTheme();
-
-  useEffect(() => {
-    document.documentElement.classList.toggle("dark", theme === "dark");
-  }, [theme]);
-
   return (
     <html lang="pt-BR">
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-              if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-                document.documentElement.classList.add('dark')
-              } else {
-                document.documentElement.classList.remove('dark')
-              }
-        `,
-        }}
-      />
       <body
-        className={`${archivo.className} ${zonaProBold.variable} ${zonaProLight.variable} ${inter.variable} bg-light text-dark dark:bg-dark-light dark:text-light antialiased`}
+        className={`${archivo.className} ${zonaProBold.variable} ${zonaProLight.variable} ${inter.variable} antialiased`}
       >
         <h1 className="sr-only">Gerenciador de Finanças</h1>
         {children}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,21 +1,12 @@
 "use client";
-import { useEffect } from "react";
-
-import { useTheme } from "../hooks/useTheme";
 
 import HeroSection from "../components/ui/Homepage/HeroSection";
 import Navbar from "../components/ui/Homepage/Navbar";
 
 export default function Home() {
-  const { theme } = useTheme();
-
-  useEffect(() => {
-    document.documentElement.classList.toggle("dark", theme === "dark");
-  }, [theme]);
-
   return (
     <>
-      <main className="text-black bg-white dark:text-white dark:bg-black h-screen w-full">
+      <main className="h-screen w-full bg-light text-dark dark:bg-white/10 dark:text-white">
         <Navbar />
         <HeroSection />
       </main>
