26e95b86360582dec8c76811a43827713d441bcf
leuzaoo <leuzaoocontato@gmail.com>
2025-08-18 09:12:13 +0100

    delete: navbar from dashboard page

--- frontend/src/app/(private)/layout.tsx
+++ frontend/src/app/(private)/layout.tsx
@@ -2,7 +2,6 @@
 
 import BottomNav from "@/src/components/ui/BottomNav";
 import Sidebar from "@/src/components/ui/Sidebar";
-import Navbar from "@/src/components/ui/Navbar";
 
 export default function Layout({
   children,
@@ -11,11 +10,7 @@ export default function Layout({
     <main className="flex h-screen w-full">
       <Sidebar />
       <div className="flex flex-1 flex-col overflow-hidden">
-        <header className="flex-shrink-0">
-          <Navbar />
-        </header>
-
-        <main className="border-dark/20 2md:pb-0 dark:bg-light/10 dark:border-light/10 flex-1 overflow-y-auto border-t bg-light p-4 pb-20 text-dark transition-all duration-1000 dark:text-light">
+        <main className="flex-1 overflow-y-auto border-t border-dark/20 bg-light p-4 pb-20 text-dark transition-all duration-1000 dark:border-light/10 dark:bg-light/10 dark:text-light 2md:pb-0">
           {children}
         </main>
       </div>
