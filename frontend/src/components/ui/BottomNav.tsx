dbda3109189e88b5394f6039e41c7afedd2fd829
leuzaoo <leuzaoocontato@gmail.com>
2025-08-18 09:27:26 +0100

    feat: add theme color to bottom navbar

--- frontend/src/components/ui/BottomNav.tsx
+++ frontend/src/components/ui/BottomNav.tsx
@@ -8,19 +8,19 @@ const menuItems = [
   {
     id: 0,
     href: "/dashboard",
-    icon: <LayoutDashboardIcon size={32} strokeWidth={1.5} />,
+    icon: <LayoutDashboardIcon size={20} strokeWidth={1.5} />,
     label: "Início",
   },
   {
     id: 1,
     href: "/wallets",
-    icon: <WalletIcon size={32} strokeWidth={1.5} />,
+    icon: <WalletIcon size={20} strokeWidth={1.5} />,
     label: "Cartões",
   },
   {
     id: 2,
     href: "/profile",
-    icon: <UserIcon size={32} strokeWidth={1.5} />,
+    icon: <UserIcon size={20} strokeWidth={1.5} />,
     label: "Perfil",
   },
 ];
@@ -29,7 +29,7 @@ export default function BottomNav() {
   const pathname = usePathname() || "/dashboard";
 
   return (
-    <nav className="fixed bottom-0 left-0 right-0 z-50 flex items-center justify-around bg-dark/10 p-2 backdrop-blur-sm 2md:hidden">
+    <nav className="fixed bottom-0 left-0 right-0 z-50 flex items-center justify-around bg-white p-2 transition-all duration-1000 dark:bg-dark 2md:hidden">
       <h1 className="sr-only">Navbar inferior</h1>
       {menuItems.map((item) => {
         const isActive =
@@ -38,12 +38,16 @@ export default function BottomNav() {
           <Link
             key={item.id}
             href={item.href}
-            className={`flex w-16 flex-col items-center justify-center rounded-lg py-1 transition-colors ${
-              isActive ? "bg-light text-dark" : "text-light hover:bg-light/10"
+            className={`flex w-16 flex-col items-center justify-center rounded-lg transition-all duration-1000 ${
+              isActive
+                ? "text-dark dark:text-light"
+                : "text-dark/40 hover:bg-light/10 dark:text-light/40 dark:hover:bg-dark/10"
             } `}
           >
             {item.icon}
-            <span className={`text-xs ${isActive && "font-bold"}`}>
+            <span
+              className={`text-[10px] transition-all duration-1000 ${isActive && "font-semibold"}`}
+            >
               {item.label}
             </span>
           </Link>
