6d780d72378a0f49c265827ec80bb58973463d15
leuzaoo <leuzaoocontato@gmail.com>
2025-09-01 09:41:21 +0100

    fix: update InputField styles for improved border visibility and focus effects

--- frontend/src/components/ui/InputField.tsx
+++ frontend/src/components/ui/InputField.tsx
@@ -28,7 +28,7 @@ function InputField({
         type={type}
         disabled={disabled}
         placeholder={placeholder}
-        className={`w-full rounded-md border border-dark/10 py-2 pl-2 text-dark outline-none dark:border-light/10 dark:bg-dark-light dark:text-white ${image && "pl-10"} ${disabled ? "cursor-not-allowed opacity-50" : ""}`}
+        className={`w-full rounded-md border border-dark/20 py-2 pl-2 text-dark shadow-sm outline-none focus:ring-1 focus:ring-blue-500 dark:border-light/10 dark:bg-dark-light dark:text-white ${image && "pl-10"} ${disabled ? "cursor-not-allowed opacity-50" : ""}`}
       />
       {image && (
         <span className="absolute left-2 top-1/2 -translate-y-1/2 opacity-50">
