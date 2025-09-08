a8cf4add0d37b79ca83ebbc2d4fd9ecfa680c6e7
leuzaoo <leuzaoocontato@gmail.com>
2025-09-08 09:39:15 +0100

    feat: refactor DashboardHeader layout; improve structure and alignment of user greeting and theme switch

--- frontend/src/components/ui/DashboardHeader.tsx
+++ frontend/src/components/ui/DashboardHeader.tsx
@@ -3,15 +3,21 @@ import { UserCircle2Icon } from "lucide-react";
 
 import { useUserStore } from "@/src/store/useUserStore";
 
+import ThemeSwitch from "./ThemeSwitch";
+
 const DashboardHeader = () => {
   const { profile: user } = useUserStore();
 
   return (
-    <div className="mb-5 flex w-full items-center gap-2">
-      <UserCircle2Icon size={52} strokeWidth={1} />
-      <p className="text-lg">
-        Olá, <span className="font-bold">{user?.firstName}</span>.
-      </p>
+    <div className="mb-5 flex w-full items-center justify-between">
+      <div className="flex items-center gap-2">
+        <UserCircle2Icon size={52} strokeWidth={1} />
+        <p className="text-lg">
+          Olá, <span className="font-bold">{user?.firstName}</span>.
+        </p>
+      </div>
+
+      <ThemeSwitch />
     </div>
   );
 };
